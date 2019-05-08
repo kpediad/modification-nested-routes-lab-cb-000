0ea3d5f4ef159113f990662b4ca5f166636596b8
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-08 14:14:14 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,11 +1,11 @@
 module SongsHelper
   def artist_select(song, form)
     if song.artist.nil? then
-      form.label "song[artist_id]", "Artist"
+      label_tag "song[artist_id]"
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       hidden_field_tag "song[artist_id]", song.artist.id
-      label_tag "song[artist_name]", "Artist Name"
+      label_tag "song[artist_name]"
       text_field_tag "song[artist_name]", song.artist.name
     end
   end
