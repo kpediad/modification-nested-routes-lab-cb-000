da5797c91e0116f456b5211df71c83d87b4f6dd1
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-08 14:03:33 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -4,7 +4,7 @@ module SongsHelper
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       hidden_field_tag "song[artist_id]", song.artist.id
-      label_tag "song[artist_name]"
+      label_tag "song[artist_name]", "Artist Name"
       text_field_tag "song[artist_name]", song.artist.name
     end
   end
